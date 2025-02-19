6bd1d36b0c96266799453be1ca585739c700fa70
heather_sutkus <heather_sutkus@localhost>
2025-02-19 17:21:43 -0600

    flag!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,9 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.yellow);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		StdDraw.filledEllipse(0.6, 0.6, 0.2, 0.1);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,63 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		
+		if (shapeType .equals ("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			Color shapeColor = new Color(red, green, blue);
+			StdDraw.setPenColor(shapeColor);
+			if (filled) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+
+		}
+		if (shapeType .equals ("triangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x2 = in.nextDouble();
+			
+			double[] xCoord = {x, x1, x2};
+			double[] yCoord = {y, y1, y2};
+			
+			Color shapeColor = new Color(red, green, blue);
+			StdDraw.setPenColor(shapeColor);
+			if (filled) {
+				StdDraw.filledPolygon(xCoord, yCoord);
+			}
+			else {
+				StdDraw.polygon(xCoord, yCoord);
+			}
+			
+		}
+		if (shapeType .equals ("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			Color shapeColor = new Color(red, green, blue);
+			StdDraw.setPenColor(shapeColor);
+			if (filled) {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+
+		}
 		
 	}
 }
